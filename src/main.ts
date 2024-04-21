c36c43fd1bdbc516794835c9a06f00d99e501c0b
Nekocatso <3093490690@qq.com>
2024-04-21 15:52:50 +0800

    Add Vue and mavon-editor imports, and initialize Vue instance in main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,26 @@ import '@/assets/css/element.scss'
 import '@/assets/css/style.scss'
 import '@/assets/css/mavon.scss'
 
+import Vue from 'vue'
 import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
+// use
+Vue.use(mavonEditor)
+new Vue({
+    'el': '#main',
+    data() {
+        return { value: '' }
+    }
+})
 // import App from './App.vue' 
 import App from '@/App.vue'
 import router from './router/index';
 import store from "@/store/index"
 //此处修改了扩展名
+import axios from './http'
+// import Editor from '-editor'
+// import installElementPlus from './plugins/element.'
+// import 'virtual:svg-icons-register'
 const app = createApp(App);
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
